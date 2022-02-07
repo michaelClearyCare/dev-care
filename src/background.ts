19ca38cf9982e2595deeb19896cbd84ba8a537c7
andrew.cherry <andrew.cherry@care.com>
2022-02-07 13:45:12 -0700

    current changes

--- src/background.ts
+++ src/background.ts
@@ -42,27 +42,71 @@ const interceptBearerToken = (req: chrome.webRequest.WebRequestHeadersDetails) =
 }
 
 const fetchGraphqlJwtBearerToken = (bearerToken: string) => {
-  // const jwtRequest = `curl --insecure -v -H 'Host: oathkeeper-httpbin.useast1.dev.omni.carezen.net' https://useast1-istioingressgw-int.dev.omni.carezen.net/headers -H 'Authorization: ${bearerToken}'`
-  fetch("https://useast1-istioingressgw-int.dev.omni.carezen.net/", {
-    headers: {
-      Authorization: bearerToken,
-      Host: "oathkeeper-httpbin.useast1.dev.omni.carezen.net"
-    }
-  }).then(res => {
-      console.log(res);
-    });
-}
+//   var headers = new Headers();
+//   headers.append("Host", "oathkeeper-httpbin.useast1.dev.omni.carezen.net");
+//   headers.append("Authorization", `${bearerToken}`);
 
-if (chrome.webRequest) {
-  // Intercept Bearer Token Listener - passes message to content script to store
-  chrome.webRequest.onBeforeSendHeaders.addListener( req => {
-    interceptBearerToken(req)
-  }, { urls: ['https://*.carezen.net/api/graphql'] }, ['requestHeaders'])
-  
-  // Amplitude Logging Listener - passes message on to devtools
-  chrome.webRequest.onBeforeRequest.addListener( details => {
-    const amplitudeEvents = JSON.parse(details.requestBody.formData.e[0])
-    sendContentMessage({ type: ContentMessageTypes.AMPLITUDE_LOGGING, payload: amplitudeEvents })
-  
-  }, {urls: ['https://*.amplitude.com/*']}, ['requestBody'])
+//   const fetch = require('node-fetch')
+//   const https = require('https')
+
+//   const httpsAgent = new https.Agent({
+//     rejectUnauthorized: false
+//   })
+
+//   console.log('Agent value:', httpsAgent)
+
+//   var requestOptions = {
+//     method: 'GET',
+//     headers: headers,
+//     agent: httpsAgent,
+//     redirect: 'follow' as RequestRedirect
+//   };
+
+// fetch("https://useast1-istioingressgw-int.dev.omni.carezen.net/headers", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
+// }
+
+  // const request = require('request');
+  // const myArgs = process.argv.slice(2);
+  // ​
+  // const headers = {
+  //     'Host': 'envoy-httpbin.useast1.dev.omni.carezen.net',
+  //     'authorization': `${bearerToken}`
+  // };
+  // ​
+  // const options = {
+  //     url: 'https://useast1-istioingressgw-int.dev.omni.carezen.net/headers',
+  //     headers: headers
+  // };
+  // ​
+  // function callback(error, response, body) {
+  //     if (!error && response.statusCode == 200) {
+  //         const bodyJSON = JSON.parse(body)
+  //         const bearerToken = bodyJSON?.headers?.Authorization;
+  //         console.log({bearerToken})
+  //         console.log("response " + {response})
+  //     }
+  // ​
+  //     if(error){
+  //         console.log({error})
+  //     }
+  // }
+  // ​
+  // request(options, callback);
+
+  if (chrome.webRequest) {
+    // Intercept Bearer Token Listener - passes message to content script to store
+    chrome.webRequest.onBeforeSendHeaders.addListener( req => {
+      interceptBearerToken(req)
+    }, { urls: ['https://*.carezen.net/api/graphql'] }, ['requestHeaders'])
+    
+    // Amplitude Logging Listener - passes message on to devtools
+    chrome.webRequest.onBeforeRequest.addListener( details => {
+      const amplitudeEvents = JSON.parse(details.requestBody.formData.e[0])
+      sendContentMessage({ type: ContentMessageTypes.AMPLITUDE_LOGGING, payload: amplitudeEvents })
+    
+    }, {urls: ['https://*.amplitude.com/*']}, ['requestBody'])
+  }
 }
